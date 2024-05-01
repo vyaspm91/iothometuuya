408985e7a4342752e767a984be9dc331105504e4
pratik <pratik.vyas@uleeco.com>
2024-05-01 18:30:16 +0530

    Integrated IOS sdk

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,8 +4,11 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    _ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    
+//    ThingSmartSDK.sharedInstance()?.start(withAppKey: rwwjd7hx5kfwhw8g7gdd,secretKey: fnwyumuvcm5rwd3m5jdapfmpje9cdnrd);
+//    
+//    [[ThingSmartSDK sharedInstance] startWithAppKey:<rwwjd7hx5kfwhw8g7gdd> secretKey:<fnwyumuvcm5rwd3m5jdapfmpje9cdnrd>]
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
