9ddf3a858d602489595a92282297c430ea56be98
pratik <pratik.vyas@uleeco.com>
2024-05-15 16:16:04 +0530

    Added integration files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,14 +3,16 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    
-//    ThingSmartSDK.sharedInstance()?.start(withAppKey: rwwjd7hx5kfwhw8g7gdd,secretKey: fnwyumuvcm5rwd3m5jdapfmpje9cdnrd);
+
+
+//  override func application(
+//    _ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
 //    
-//    [[ThingSmartSDK sharedInstance] startWithAppKey:<rwwjd7hx5kfwhw8g7gdd> secretKey:<fnwyumuvcm5rwd3m5jdapfmpje9cdnrd>]
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+////    ThingSmartSDK.sharedInstance()?.start(withAppKey: rwwjd7hx5kfwhw8g7gdd,secretKey: fnwyumuvcm5rwd3m5jdapfmpje9cdnrd);
+////    
+////    [[ThingSmartSDK sharedInstance] startWithAppKey:<rwwjd7hx5kfwhw8g7gdd> secretKey:<fnwyumuvcm5rwd3m5jdapfmpje9cdnrd>]
+//  ) -> Bool {
+//    GeneratedPluginRegistrant.register(with: self)
+//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//  }
 }
